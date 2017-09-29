23d42cb94f57c37afbd1bbc4e4fd8f6cdbd078c3
Jerrad Monagan <jerradmonagan@gmail.com>
2017-09-28 20:06:20 -0400

    added logs for voice channel

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// started by using Adam Gaskins tutorial @ https://www.youtube.com/watch?v=9CDPw1lCkJ8&t=23s
-//   const commando = require('discord.js-commando'); from part 2 of video
-//   const bot = new commando.Client(); from part 2 of video
-//   const bot = new commando.Client(); from part 2 of video
-//   bot.registry.registerGroup('random', 'Random');
-//   bot.registry.registerDefaults(); //list of help commands
-//   bot.registry.registerCommandsIn(__dirname +"/commands");
+
 
 const Discord= require('discord.js');  // from part 1 of video
 const bot = new Discord.Client(); //from part1 of video
 const settings = require('./settings.json');
+var fs = require('fs');
 var prefix ="~";
 
 bot.on('ready', () => {
@@ -22,17 +17,29 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
   // User Joins a voice channel
   if(oldUserChannel === undefined && newUserChannel !== undefined) {
     var d = new Date();
-    console.log("User "+ newMember.displayName +" has joined the channel "+newMember.voiceChannel.name+" @" + d);
+    console.log("User "+ newMember.displayName +" has joined the channel "+newMember.voiceChannel.name+" on "+newMember.guild+" @" + d);
     var join = ("User "+ newMember.displayName +" has joined the channel ");
     bot.channels.get('363082234565099522').send(join);
+    var joinLog = ("User "+ newMember.displayName +" has joined the channel "+newMember.voiceChannel.name+" on "+newMember.guild+" @" + d);
+    fs.appendFile("./logs", joinLog+"\r\n", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+});
   }//end if
 // User leaves a voice channel
   else if(newUserChannel === undefined)
   {
     var d = new Date();
-    console.log("User "+ newMember.displayName +" has left the channel "+oldMember.voiceChannel.name+ " @" + d);
+    console.log("User "+ newMember.displayName +" has left the channel "+oldMember.voiceChannel.name+ " on "+oldMember.guild+" @" + d);
     var leave = ("User "+ newMember.displayName +" has left the channel ");
     bot.channels.get('363082234565099522').send(leave);
+    var leaveLog = ("User "+ oldMember.displayName +" has left the channel "+oldMember.voiceChannel.name+" on "+oldMember.guild+" @" + d);
+    fs.appendFile("./logs", leaveLog+"\r\n", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+});
   }//end else
 })
 
@@ -41,18 +48,18 @@ if (message.content === prefix+'ping') {
   //message.reply('pong');
   message.channel.send('pong');}
 });
-bot.on('guildMemberSpeaking', (member, speaking) => {
-
-  let guild = member.guild;
-
-  if (member.speaking) {
-
-    //guild.defaultchannel.send("is speaking!");
-    console.log("heya");
-
-  }
-
-});
+// bot.on('guildMemberSpeaking', (member, speaking) => {
+//
+//   let guild = member.guild;
+//
+//   if (member.speaking) {
+//
+//     //guild.defaultchannel.send("is speaking!");
+//     console.log("heya");
+//
+//   }
+//
+// });
 
 
 bot.on('message', message => {
